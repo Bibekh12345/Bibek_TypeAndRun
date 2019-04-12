76db899d115fc4c2bc05df1f19502aac19bd19bb
Softwarica College Lab <softwarica@softwarica.com>
2019-04-12 17:04:34 +0545

    Changes

---
+++ app/src/main/java/com/bibek_typeandrun/Bibek_CreateCars.java
@@ -0,0 +1,25 @@
+package com.bibek_typeandrun;
+
+public class Bibek_CreateCars {
+
+    private String make, year, color, price, engine, Answer;
+    private int count;
+
+    public Bibek_CreateCars(String make, String year, String color, String price, String engine, int count) {
+        this.make = make;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.engine = engine;
+        this.count = count;
+    }
+
+    public String CreatesCar(){
+
+
+        Answer= "The is Vehicle No. "+count+"\n"+" Manufacturer: "+make+" Current Value: "+price+ " Effective engine Size: "+engine+"\n";
+        return Answer;
+
+    }
+
+}
